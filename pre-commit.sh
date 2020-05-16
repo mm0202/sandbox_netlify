7a2d1ecf42ad46e9bbf70b56f8f7d830f5850fde
mm0202 <for_etc@hotmail.com>
2020-05-17 06:21:00 +0900

    SB-201 tune

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,4 @@
+stage=$1
 cp src/index.html public/index.html
-sed -i 's/{stage}/deploy-preview/g' public/index.html
-sed -i "s/{date}/`date`/g" public/index.html
-
-git add public
+sed -i "s/{stage}/$stage/g" public/index.html
+sed -i "s/{date}/$(date)/g" public/index.html
